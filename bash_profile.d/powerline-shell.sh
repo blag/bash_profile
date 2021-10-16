eec70ce143d62b351044e4ea578fe426ad5fc0c1
blag <blag@users.noreply.github.com>
2021-10-15 17:57:34 -0700

    Add support for the powerline-go alternative to powerline-shell

--- bash_profile.d/powerline-shell.sh
+++ bash_profile.d/powerline-shell.sh
@@ -11,12 +11,28 @@
 # Dependencies: direnv, local
 
 if [[ -x "${LOCAL_DIR}/bin/powerline-shell.py" ]]; then
+	POWERLINE_SHELL_EXEC="${LOCAL_DIR}/bin/powerline-shell.py"
+elif which -s powerline-go 1>/dev/null; then
+	POWERLINE_SHELL_EXEC=$(which powerline-go)
+fi
+
+if [[ -n "$POWERLINE_SHELL_EXEC" ]]; then
 	if [[ -z "$POWERLINE_SHELL_MODE" ]]; then
 		POWERLINE_SHELL_MODE=flat
 	fi
 
 	function _update_ps1() {
-		PS1="$(${LOCAL_DIR}/bin/powerline-shell.py --mode ${POWERLINE_SHELL_MODE} $? 2>${LOG_FILE})"
+		PS1="$(${POWERLINE_SHELL_EXEC} --mode ${POWERLINE_SHELL_MODE} \
+			                           --error $? \
+			                           --jobs $(jobs -p | wc -l) \
+			                           2>${LOG_FILE})"
+
+		# Uncomment the following line to automatically clear errors after showing
+		# them once. This not only clears the error for powerline-go, but also for
+		# everything else you run in that shell. Don't enable this if you're not
+		# sure this is what you want.
+
+		#set "?"
 	}
 
 	# We intentionally do not enclose the not comparison in quotes - we are
