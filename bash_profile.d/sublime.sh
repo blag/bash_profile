2e04b129c26b2663b7ce050f06ea5fdd48a82891
blag <blag@users.noreply.github.com>
2021-10-15 17:51:30 -0700

    Add support for Sublime Merge

--- bash_profile.d/sublime.sh
+++ bash_profile.d/sublime.sh
@@ -10,13 +10,14 @@
 #
 # Dependencies: homebin
 
-SUBLIME_COMMAND="${SUBLIME_INSTALL_DIR}/bin/subl"
-SUBLIME_SYMLINK="$USER_PREFIX_BIN/subl"
-if [[ -x "$SUBLIME_INSTALL_DIR" ]]; then
+SUBLIME_TEXT_COMMAND="${SUBLIME_TEXT_INSTALL_DIR}/bin/subl"
+SUBLIME_TEXT_SYMLINK="$USER_PREFIX_BIN/subl"
+
+if [[ -x "$SUBLIME_TEXT_INSTALL_DIR" ]]; then
 	# Check for the subl command and symlink it
-	if [[ -x "$SUBLIME_COMMAND" && ! -e "$USER_PREFIX_BIN/subl" ]]; then
-		ln -s "$SUBLIME_COMMAND" "$SUBLIME_SYMLINK"
-		chmod +x "$SUBLIME_SYMLINK"
+	if [[ -x "$SUBLIME_TEXT_COMMAND" && ! -e "$USER_PREFIX_BIN/subl" ]]; then
+		ln -s "$SUBLIME_TEXT_COMMAND" "$SUBLIME_TEXT_SYMLINK"
+		chmod +x "$SUBLIME_TEXT_SYMLINK"
 	fi
 
 	EDITOR="subl -n -w"
@@ -26,8 +27,8 @@ else
 	# If the user removed Sublime for some reason
 
 	# Remove the symlink
-	if [[ -L "$SUBLIME_SYMLINK" ]]; then
-		rm "$SUBLIME_SYMLINK"
+	if [[ -L "$SUBLIME_TEXT_SYMLINK" ]]; then
+		rm "$SUBLIME_TEXT_SYMLINK"
 	fi
 
 	# Unset EDITOR
@@ -36,4 +37,27 @@ else
 	fi
 
 	EXPORTS+=("EDITOR")
+fi
+
+SUBLIME_MERGE_COMMAND="${SUBLIME_MERGE_INSTALL_DIR}/bin/smerge"
+SUBLIME_MERGE_SYMLINK="$USER_PREFIX_BIN/smerge"
+
+if [[ -x "$SUBLIME_MERGE_INSTALL_DIR" ]]; then
+	# Check for the smerge command and symlink it
+	if [[ -x "$SUBLIME_MERGE_COMMAND" && ! -e "$USER_PREFIX_BIN/smerge" ]]; then
+		ln -s "$SUBLIME_MERGE_COMMAND" "$SUBLIME_MERGE_SYMLINK"
+		chmod +x "$SUBLIME_MERGE_SYMLINK"
+	fi
+
+	EDITOR="smerge"
+
+	EXPORTS+=("EDITOR")
+else
+	# If the user removed Sublime for some reason
+
+	# Remove the symlink
+	if [[ -L "$SUBLIME_MERGE_SYMLINK" ]]; then
+		rm "$SUBLIME_MERGE_SYMLINK"
+	fi
+
 fi
--- bash_profile.d/sublime/osx.sh
+++ bash_profile.d/sublime/osx.sh
@@ -1,3 +1,4 @@
 
 
-SUBLIME_INSTALL_DIR="/Applications/Sublime Text.app/Contents/SharedSupport"
+SUBLIME_TEXT_INSTALL_DIR="/Applications/Sublime Text.app/Contents/SharedSupport"
+SUBLIME_MERGE_INSTALL_DIR="/Applications/Sublime Merge.app/Contents/SharedSupport"
