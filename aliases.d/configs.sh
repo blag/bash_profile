8fabb70cbfcdb57cc267f28d2ea1b95103cd5bb6
blag <blag@users.noreply.github.com>
2021-10-21 13:18:26 -0700

    Silence chmod stdout and fix (en|dis)able_configs aliases

--- aliases.d/configs.sh
+++ aliases.d/configs.sh
@@ -9,8 +9,8 @@
 # After the function is defined, it is called, and the arguments are passed to
 # it
 
-alias  enable_config='__f() { chmod +x $HOME/.bash_profile.d/$1.sh; chmod +x $HOME/.bash_profile.d/$1/*.sh 2>/dev/null; unset -f __f; }; __f'
-alias disable_config='__f() { chmod -x $HOME/.bash_profile.d/$1.sh; chmod -x $HOME/.bash_profile.d/$1/*.sh 2>/dev/null; unset -f __f; }; __f'
+alias  enable_config='__f() { chmod +x $HOME/.bash_profile.d/$1.sh; chmod +x $HOME/.bash_profile.d/$1/*.sh 1>/dev/null 2>/dev/null; unset -f __f; }; __f'
+alias disable_config='__f() { chmod -x $HOME/.bash_profile.d/$1.sh; chmod -x $HOME/.bash_profile.d/$1/*.sh 1>/dev/null 2>/dev/null; unset -f __f; }; __f'
 
-alias  enable_configs='__f() { for arg in $@; do  enable_alias $arg; done; unset -f __f; }; __f'
-alias disable_configs='__f() { for arg in $@; do disable_alias $arg; done; unset -f __f; }; __f'
+alias  enable_configs='__f() { for arg in $@; do  enable_config $arg; done; unset -f __f; }; __f'
+alias disable_configs='__f() { for arg in $@; do disable_config $arg; done; unset -f __f; }; __f'
